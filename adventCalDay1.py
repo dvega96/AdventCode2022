cfbbc932045fc0577472472adee0f194b4a2609d
dvega96 <43462469+dvega96@users.noreply.github.com>
2023-01-02 01:10:54 -0800

    Add files via upload

---
+++ adventCalDay1.py
@@ -0,0 +1,30 @@
+firstVal = 0
+secondVal = 0
+thirdVal = 0
+currentVal = 0
+file = open("DayOneTest.txt")
+
+for line in file:
+    if line == '\n':
+        if currentVal > firstVal:
+            thirdVal = secondVal
+            secondVal = firstVal
+            firstVal = currentVal
+        elif currentVal > secondVal:
+            thirdVal = secondVal
+            secondVal = currentVal
+        elif currentVal > thirdVal:
+            thirdVal = currentVal
+        currentVal = 0
+    else:
+        currentVal += int(line)
+if currentVal > firstVal:
+    thirdVal = secondVal
+    secondVal = firstVal
+    firstVal = currentVal
+elif currentVal > secondVal:
+    thirdVal = secondVal
+    secondVal = currentVal
+elif currentVal > thirdVal:
+    thirdVal = currentVal
+print(firstVal + secondVal + thirdVal)
